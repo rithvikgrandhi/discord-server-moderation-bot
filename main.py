c1da796eb37704b1d0b905a8374d1b37d2f5f87b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2021-09-26 18:09:53 +0530

    made changes to snipe
    made deleted message to get send in logs channel and fixed it completely and added embed too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import asyncio
 from discord.ext import commands
 from webserver import keep_alive
 
@@ -35,15 +36,22 @@ snipe_message_content = {}
 async def on_message_delete(message):
      snipe_message_author[message.channel.id] = message.author
      snipe_message_content[message.channel.id] = message.content
-     
+     channellogs=client.get_channel(891551602509488128)
+     embed=discord.Embed(title=str(message.author)+"deleted the message in "+str(message.channel),color=discord.Color.random())
+     embed.add_field(name="Message:",value=message.content,inline=False)
+     await channellogs.send(embed=embed)
+    
+     await asyncio.sleep(120)
+    
      del snipe_message_author[message.channel.id]
      del snipe_message_content[message.channel.id]
+        
 
 @client.command(name = 'snipe')
 async def snipe(ctx):
     channel = ctx.channel
     try: #This piece of code is run if the bot finds anything in the dictionary
-        em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
+        em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id],color=discord.Color.random())
         em.set_footer(text = f"This message was sent by {snipe_message_author[channel.id]}")
         await ctx.send(embed = em)
     except: #This piece of code is run if the bot doesn't find anything in the dictionary
