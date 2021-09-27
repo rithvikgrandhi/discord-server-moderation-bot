6a00fcedb4262c58f7390b2970f8eac1048e2db8
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2021-09-27 08:36:57 +0530

    added many commands

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import os
 import discord
 import asyncio
 from discord.ext import commands
+from discord.ext import tasks
+from discord.utils import get
 from webserver import keep_alive
 
 #000000000000000000000000000000000
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix="pes ",intents=intents)
 
-client = commands.Bot(command_prefix="pes ")
 @client.event
 async def on_ready():
     print("the bot is ready")
+
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Ping: {round(client.latency * 1000)} ms") 
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Ping: {round(client.latency * 1000)} ms")
+
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hi")
+    await ctx.send(f'Hello {ctx.author.mention}')
+    await ctx.author.send("hello,I hope you are doing good")
+
+@client.command()
+async def greet(ctx, name, *,greeting):
+    await ctx.send(f"{greeting} {name}")
 
 @client.command()
 async def noob(ctx):
@@ -28,6 +43,101 @@ async def pride(ctx):
 @client.command()
 async def god(ctx):
     await ctx.send("that is <@676050443180179468>")
+    
+#clear command
+@client.command(aliases=['purge','p','c'])
+async def clear(ctx, amount=1):
+    perms = ctx.channel.permissions_for(ctx.author)
+    if perms.manage_messages:
+        await ctx.channel.purge(limit=amount+1)
+    else: 
+        await ctx.send("Don't  have permission to delete messages here")
+    
+
+#changing nicknames command
+@client.command()
+async def nick(ctx, member: discord.Member, *,newname: str):
+    # Gets permissions that the message sender has
+    perms = ctx.channel.permissions_for(ctx.author)
+    if perms.manage_nicknames:
+        # If the sender can edit nicknames, edit
+        await member.edit(nick=newname)
+    else: 
+        await ctx.send("Don't  have permissions to change nicknames")
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Required arguments not given")
+        
+#server information command
+@client.command()
+async def serverinfo(ctx):
+    name=ctx.guild.name
+    description=ctx.guild.description
+    region=ctx.guild.region
+    icon=ctx.guild.icon_url
+    memberCount=ctx.guild.member_count
+    owner=ctx.guild.owner
+
+    embed=discord.Embed(title=name+"Server Information",description=description,color=discord.Color.blue())
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Description",value=description,inline=False)
+    embed.add_field(name="Owner",value=owner,inline=False)
+    embed.add_field(name="Region",value=region,inline=False)
+    embed.add_field(name="Member count",value=memberCount,inline=False)
+    await ctx.send(embed=embed)
+
+#kick command
+@client.command()
+@commands.has_role(834694539142103041)
+async def kick(ctx,member:discord.Member,*,reason="No reason given"):
+    await member.send("You were kicked from "+ctx.guild.name+". Reason: "+reason)
+    await member.kick(reason=reason)
+
+@kick.error
+async def kick_error(ctx,error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You don't have the permssion to use this command")
+
+#ban command
+@client.command()
+@commands.has_role(834694539142103041)
+async def ban(ctx,member:discord.Member,*,reason="No reason given"):
+    await member.send("You were banned from "+ctx.guild.name+". Reason: "+reason)
+    await member.ban(reason=reason)
+
+@ban.error
+async def ban_error(ctx,error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You don't have the permssion to use this command")
+
+#unban command
+@client.command()
+@commands.has_role(834694539142103041)
+async def unban(ctx,*,member):
+    banned_members=await ctx.guild.bans()
+    for person in banned_members:
+        user=person.user
+        if member==str(user):
+            await ctx.guild.unban(user)
+
+@unban.error
+async def unban_error(ctx,error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You don't have the permssion to use this command")
+
+#role giving command
+@client.command(pass_context=True)
+@commands.has_role(834694539142103041) 
+async def giveRole(ctx,member:discord.Member,role:discord.Role):
+    await member.add_roles(role)
+
+@giveRole.error
+async def giveRole_error(ctx,error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You don't have the permssion to use this command")
+
 
 snipe_message_author = {}
 snipe_message_content = {}
